4bca56be5cc5a9289bafce58857cc58bd1f102bb
Masoud Moradi <147343920+massi-web-app@users.noreply.github.com>
2024-11-13 15:49:15 +0300

    implement manage layout authentication && manage route (register && login)

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,23 @@
 import {createBrowserRouter} from "react-router-dom";
 import Login from "./features/identity/components/login.jsx";
 import Register from "./features/identity/components/register.jsx";
+import IndentifyLayout from "./layouts/indentify-layout.jsx";
 
 const router = createBrowserRouter([
     {
-        path: 'login',
-        element: <Login/>
+        element: <IndentifyLayout/>,
+        children: [
+            {
+                path: 'login',
+                element: <Login/>
+            },
+            {
+                path: 'register',
+                element: <Register/>
+            }
+        ],
     },
-    {
-        path: 'register',
-        element: <Register/>
-    }
+
 ]);
 
 export default router;
